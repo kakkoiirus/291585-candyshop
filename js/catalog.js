582119452477b8942c30d58f5b9abe5931c174ca
Valentin <bagadin@inbox.ru>
2018-09-29 23:14:15 +0500

    Частично: задание 25 Личный проект: перламутровые пуговицы

--- js/catalog.js
+++ js/catalog.js
@@ -4,22 +4,125 @@
 
   var products;
 
-  var onLoadProductsSuccess = function (productsData) {
-    products = productsData;
+  var kindFilter = [
+    {
+      id: 'filter-icecream',
+      name: 'Мороженое'
+    },
+    {
+      id: 'filter-soda',
+      name: 'Газировка'
+    },
+    {
+      id: 'filter-gum',
+      name: 'Жевательная резинка'
+    },
+    {
+      id: 'filter-marmalade',
+      name: 'Мармелад'
+    },
+    {
+      id: 'filter-marshmallows',
+      name: 'Зефир'
+    }
+  ];
+
+  var nutritionFilter = [
+    {
+      id: 'filter-sugar-free',
+      name: 'sugar'
+    },
+    {
+      id: 'filter-vegetarian',
+      name: 'vegetarian'
+    },
+    {
+      id: 'filter-gluten-free',
+      name: 'gluten'
+    }
+  ];
+
+  var specialFilter = [
+    {
+      id: 'filter-favorite'
+    },
+    {
+      id: 'filter-availability'
+    }
+  ];
+
+  kindFilter.forEach(function (filter) {
+    filter.inputElement = document.querySelector('#' + filter.id);
+    filter.countElement = document.querySelector('#' + filter.id + '~ .input-btn__item-count');
+  });
+
+  nutritionFilter.forEach(function (filter) {
+    filter.inputElement = document.querySelector('#' + filter.id);
+    filter.countElement = document.querySelector('#' + filter.id + '~ .input-btn__item-count');
+  });
+
+  specialFilter.forEach(function (filter) {
+    filter.inputElement = document.querySelector('#' + filter.id);
+    filter.countElement = document.querySelector('#' + filter.id + '~ .input-btn__item-count');
+  });
+
+  var renderFilterCount = function (productsData) {
+    kindFilter.forEach(function (filter) {
+      filter.products = productsData.filter(function (product) {
+        return product.kind === filter.name;
+      });
+
+      filter.countElement.textContent = '(' + filter.products.length + ')';
+    });
+
+    nutritionFilter.forEach(function (filter) {
+      filter.products = productsData.filter(function (product) {
+        return product.nutritionFacts[filter.name] === true;
+      });
+
+      filter.countElement.textContent = '(' + filter.products.length + ')';
+    });
+
+    specialFilter.forEach(function (filter) {
+      filter.products = productsData.filter(function (product) {
+        return product.amount > 0;
+      });
+
+      filter.countElement.textContent = '(' + filter.products.length + ')';
+    });
+  };
+
+  var getKindFilter = function () {
+    return kindFilter;
+  };
+
+  var getNutritionFilter = function () {
+    return nutritionFilter;
+  };
+
+  var getSpecialFilter = function () {
+    return specialFilter;
+  };
+
+  var renderProducts = function (productsData) {
     var catalogCards = document.querySelector('.catalog__cards');
     catalogCards.classList.remove('catalog__cards--load');
-
-    var catalogLoad = document.querySelector('.catalog__load');
-    catalogLoad.classList.add('visually-hidden');
+    catalogCards.innerHTML = '';
 
     var catalogCardTemplate = document.querySelector('#card').content.querySelector('.catalog__card');
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < products.length; i++) {
-      fragment.appendChild(window.catalogItem.getProductCard(products[i], catalogCardTemplate, i));
+    for (var i = 0; i < productsData.length; i++) {
+      fragment.appendChild(window.catalogItem.getProductCard(productsData[i], catalogCardTemplate, i));
     }
 
     catalogCards.appendChild(fragment);
+  };
+
+  var onLoadProductsSuccess = function (productsData) {
+    products = productsData;
+    renderProducts(products);
+    renderFilterCount(products);
   };
 
   var onLoadProductsError = function (message) {
@@ -33,6 +136,10 @@
   window.backend.load(onLoadProductsSuccess, onLoadProductsError);
 
   window.catalog = {
-    getProducts: getProducts
+    getProducts: getProducts,
+    getKindFilter: getKindFilter,
+    renderProducts: renderProducts,
+    getNutritionFilter: getNutritionFilter,
+    getSpecialFilter: getSpecialFilter
   };
 })();
--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  var filterForm = document.querySelector('.catalog__sidebar form');
   var catalogFilter = document.querySelector('.catalog__filter + .range');
   var rangeFilter = catalogFilter.querySelector('.range__filter');
   var rangeLine = rangeFilter.querySelector('.range__fill-line');
@@ -11,6 +12,65 @@
   var buttonWidth = rangeButtonLeft.offsetWidth;
   var leftButtonX = rangeButtonLeft.offsetLeft;
   var rightButtonX = rangeButtonRight.offsetLeft;
+
+  var uncheckFilters = function (activeSpecialFilter) {
+    window.catalog.getKindFilter().forEach(function (filter) {
+      filter.inputElement.checked = false;
+    });
+
+    window.catalog.getNutritionFilter().forEach(function (filter) {
+      filter.inputElement.checked = false;
+    });
+
+    window.catalog.getSpecialFilter().forEach(function (filter) {
+      if (filter.id !== activeSpecialFilter) {
+        filter.inputElement.checked = false;
+      }
+    });
+  };
+
+  var uncheckSpecialFilters = function () {
+    window.catalog.getSpecialFilter().forEach(function (filter) {
+      filter.inputElement.checked = false;
+    });
+  };
+
+  var onFormChange = function (evt) {
+    var products = [];
+    var checkedCount = 0;
+
+    if (evt.target.id === 'filter-availability') {
+      uncheckFilters(evt.target.id);
+      products = window.catalog.getSpecialFilter()[1].products;
+    } else if (evt.target.id === 'filter-favorite') {
+      uncheckFilters(evt.target.id);
+    } else {
+      uncheckSpecialFilters();
+
+      window.catalog.getKindFilter().forEach(function (filter) {
+        if (filter.inputElement.checked) {
+          checkedCount++;
+          products = products.concat(filter.products);
+        }
+      });
+
+      if (!checkedCount && products.length === 0) {
+        products = window.catalog.getProducts();
+      }
+
+      window.catalog.getNutritionFilter().forEach(function (filter) {
+        if (filter.inputElement.checked) {
+          products = products.filter(function (product) {
+            return product.nutritionFacts[filter.name] === true;
+          });
+        }
+      });
+    }
+
+    window.catalog.renderProducts(products);
+  };
+
+  filterForm.addEventListener('change', onFormChange);
 
   rangeButtonLeft.addEventListener('mousedown', function (evt) {
     var startCoordinate = evt.clientX;
